31fea7dba373ad7ed2e6f5dc57d7da7dfa89ca7c
dustan1 <goadustan@gmail.com>
2017-10-15 12:39:55 -0400

    AA

---
+++ MySQLTesting/MySQLTesting.cpp
@@ -0,0 +1,27 @@
+// MySQLTesting.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "mysql_connection.h"
+#include "mysql_driver.h"
+#include "mysql_error.h"
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Hello world!" << std::endl;
+
+	sql::Connection *con;
+	sql::Statement *stmt;
+
+
+
+	//delete con;
+
+
+    return 0;
+}
+
